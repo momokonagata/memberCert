a7f2b0ce9df8dce1189d23792c2ad666724ac786
momokonagata <131645391+momokonagata@users.noreply.github.com>
2024-08-07 17:41:33 +0900

    Add files via upload

---
+++ memberCert.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MemberCert is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    constructor(address initialOwner) ERC721("MemberCert", "MDC") 
+    Ownable(initialOwner) 
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://momokonagata.github.io/sample-NFT-metadata/assets/";
+    }
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+   function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }    
+}
